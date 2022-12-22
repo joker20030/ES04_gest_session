0d4a271936524bd82d779837ce1251fe1fa43f01
jolly <61207499+joker20030@users.noreply.github.com>
2022-12-22 11:41:17 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" type="text/css"href="stile_ind.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <p><h1>Welcome</h1></p>
+    
+    <a href="login.php">Login</a><br>
+    <a href="riservata.php">Vai all'area riservata del sito</a>
+    
+</body>
+</html>
